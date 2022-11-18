4954f8c7a55bbdc5a898b0763a9ab2977a037d78
RkzNew <rahamannovel@gmail.com>
2022-11-18 16:47:23 +0600

    banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import b1 from '../../../assets/banner/b1.jpeg'
+import b2 from '../../../assets/banner/b2.jpg'
+import b3 from '../../../assets/banner/b3.jpg'
+import b4 from '../../../assets/banner/b4.jpeg'
+const Banner = () => {
+    return (
+        <div className="carousel w-full py-10">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={b1} alt='' className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={b2} alt='' className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={b3} alt='' className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={b4} alt='' className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Reviews from "../../Pages/Reviews/Reviews";
 
-const Routes = () => {
-    return (
-        <div>
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },  
+            {
+                path: '/reviews',
+                element: <Reviews></Reviews>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
             
-        </div>
-    );
-};
+        ]
+    }
+]);
 
-export default Routes;
+export default router;
